42a3ed686f5adaae0680b8eb4e83ee5a7b3f0479
LeonidElkin <113133848+LeonidElkin@users.noreply.github.com>
2025-04-02 22:00:08 +0300

    test: added tests for censor distr

---
+++ pacal/test/censor_test.py
@@ -0,0 +1,28 @@
+from pacal import ConstDistr, UniformDistr
+from pacal.rv import CensoredRV, RV
+
+
+class TestCensor(object):
+    def test_censored_distr(self):
+        d = UniformDistr(0, 10)
+
+        censored = d.censor(2, 8)
+
+        assert censored.range() == (0, 10)
+        assert censored.pdf(1.9) == censored.pdf(2)
+        assert censored.pdf(8.1) == censored.pdf(8)
+
+    def test_censored_rv(self):
+        base_rv = RV(sym="X", a=0, b=10)
+        censored_rv = CensoredRV(base_rv, 4, 6)
+
+        assert censored_rv.getSegmentsCensoredTo() == (4, 6)
+        assert censored_rv.censor_a == 4
+        assert censored_rv.censor_b == 6
+
+    def test_censor_method(self):
+        d = ConstDistr(5)
+        censored = d.censor(3, 7)
+
+        assert censored.pdf(2) == censored.pdf(3)
+        assert censored.pdf(8) == censored.pdf(7)
